31e94eb941972c2a0b8c7fc04cac1c8813acb457
KuelloKompleteEdition <159363862+KuelloKompleteEdition@users.noreply.github.com>
2024-04-14 18:29:20 -0500

    Cartelera
    Agregue el componente Cuadricula_Prin que contiene la lista de peliculas en la pestaña principal
    Además agregue una API Key a The movie database

--- CP(Cine Plus)/src/Pages/Index/App.jsx
+++ CP(Cine Plus)/src/Pages/Index/App.jsx
@@ -1,9 +1,11 @@
 import {Navigation, Carousel, MovieRow} from '../Principal/Principal.jsx'
+import Cartelera from '../Principal/Cuadricula_Prin/Cuadricula_Prin.jsx'
 
 function App() {
   return (
     <>
       <Navigation />
+      <Cartelera></Cartelera>
     </>
   )
 }
---
+++ CP(Cine Plus)/src/Pages/Principal/Cuadricula_Prin/Cuadricula_Prin.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Cuadricula_Prin.css'; // Importar el archivo de estilos
+
+const Cartelera = () => {
+  const [peliculas, setPeliculas] = useState([]);
+
+  useEffect(() => {
+    const fetchPeliculas = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.themoviedb.org/3/movie/now_playing',
+          {
+            params: {
+              api_key: '3b24534de9f3e0c2935e3edd6446ad0c',
+              language: 'es',
+              page: 1,
+            },
+          }
+        );
+        setPeliculas(response.data.results);
+      } catch (error) {
+        console.error('Error al obtener las películas:', error);
+      }
+    };
+
+    fetchPeliculas();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Cartelera de películas</h1>
+      <div className="grid">
+        {peliculas.map((pelicula) => (
+          <div key={pelicula.id} className="movie">
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${pelicula.poster_path}`}
+              alt={pelicula.title}
+            />
+            <h2>{pelicula.title}</h2>
+            <p>Clasificación: {pelicula.vote_average}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cartelera;
+
