39a7b94743b919099fbbe6985e065d5dd60d7de9
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-14 18:11:29 -0500

    Añadiendo Carrusel
    Haciendo

--- CP(Cine Plus)/src/Pages/Index/App.jsx
+++ CP(Cine Plus)/src/Pages/Index/App.jsx
@@ -1,10 +1,24 @@
-import {Navigation, Carousel, MovieRow} from '../../Principal/Principal.jsx'
+import { blogs } from "../../Helpers/data/data"
+import { CardBlog } from "../Principal/Carrusel/CardBlog"
+import { Navigation } from "../Principal/Principal"
+
 
 function App() {
+  const carouselBlogs= [...blogs,...blogs]
   return (
-    <>
-      <Navigation />
-    </>
+    <div className='App'>
+        <Navigation/>
+      <div className='container my-5'>
+				<div className='overflow-x-auto whitespace-nowrap flex-row'>
+					<div className='flex whitespace-nowrap animate-scroll'>
+						{carouselBlogs.map((blog, index) => (
+      
+							<CardBlog blog={blog} key={index} />
+						))}
+					</div>
+				</div>
+			</div>
+    </div>
   )
 }
 
--- CP(Cine Plus)/src/Pages/Principal/Principal.jsx
+++ CP(Cine Plus)/src/Pages/Principal/Principal.jsx
@@ -1,7 +1,7 @@
 import './Principal.css'; // Importa el archivo de estilos CSS
 export  function Navigation() {
     return (
-    <>
+    
     <div className="BarraNav">
       <nav className="BarraNav">
         <ul>
@@ -13,34 +13,5 @@ export  function Navigation() {
         </ul>
       </nav>
     </div>
-    </>
-    );
-  }
-  
-  // Componente de carrusel de películas
-export  function Carousel() {
-    return (
-      <div className="carrusel">
-        <h2>Carrusel de Películas</h2>
-        <div className="slick-carousel">
-          <div><img src="pelicula1.jpg" alt="Película 1" /></div>
-          <div><img src="pelicula2.jpg" alt="Película 2" /></div>
-          <div><img src="pelicula3.jpg" alt="Película 3" /></div>
-          {/* Agrega más películas según sea necesario */}
-        </div>
-      </div>
-    );
-  }
-  
-  // Componente de fila de películas
-export function MovieRow() {
-    return (
-      <div className="fila">
-        {/* Películas de la fila */}
-        <div className="pelicula"><img src="pelicula4.jpg" alt="Película 4" /></div>
-        <div className="pelicula"><img src="pelicula5.jpg" alt="Película 5" /></div>
-        <div className="pelicula"><img src="pelicula6.jpg" alt="Película 6" /></div>
-        {/* Agrega más películas según sea necesario */}
-      </div>
     );
   }
