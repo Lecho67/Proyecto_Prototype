d5e5dd673c16065248e6bd59d9cc33715834819f
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-14 18:34:18 -0500

    Me arrepenti de mis actos

--- CP(Cine Plus)/src/Pages/Index/App.jsx
+++ CP(Cine Plus)/src/Pages/Index/App.jsx
@@ -4,22 +4,22 @@ import { Navigation } from "../Principal/Principal"
 
 
 function App() {
-  const carouselBlogs= [...blogs,...blogs]
-  return (
-    <div className='App'>
-        <Navigation/>
-      <div className='container my-5'>
-				<div className='overflow-x-auto whitespace-nowrap flex-row'>
+	const carouselBlogs = [...blogs, ...blogs];
+
+	return (
+		<div className='App'>
+			<Navigation />
+			<div className='container my-5'>
+				<div className='overflow-hidden w-full'>
 					<div className='flex whitespace-nowrap animate-scroll'>
 						{carouselBlogs.map((blog, index) => (
-      
 							<CardBlog blog={blog} key={index} />
 						))}
 					</div>
 				</div>
 			</div>
-    </div>
-  )
+		</div>
+	);
 }
 
 export default App
