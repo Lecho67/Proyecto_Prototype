e7e297d0b909972c0a45e2bf2429927cd500ed0a
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-14 19:20:40 -0500

    Cuadricula

--- CP(Cine Plus)/src/Pages/Principal/Cuadricula_Prin/Cuadricula_Prin.jsx
+++ CP(Cine Plus)/src/Pages/Principal/Cuadricula_Prin/Cuadricula_Prin.jsx
@@ -4,28 +4,40 @@ import './Cuadricula_Prin.css'; // Importar el archivo de estilos
 
 const Cartelera = () => {
   const [peliculas, setPeliculas] = useState([]);
+  const numeroDePeliculas = 30; // Número de películas que deseas mostrar
+  const pageSize = 20; // Tamaño de página de la API de TMDB
 
   useEffect(() => {
     const fetchPeliculas = async () => {
       try {
-        const response = await axios.get(
-          'https://api.themoviedb.org/3/movie/now_playing',
-          {
-            params: {
-              api_key: '3b24534de9f3e0c2935e3edd6446ad0c',
-              language: 'es',
-              page: 3,
-            },
-          }
-        );
-        setPeliculas(response.data.results);
+        const totalPages = Math.ceil(numeroDePeliculas / pageSize);
+        const requests = [];
+
+        for (let page = 1; page <= totalPages; page++) {
+          requests.push(
+            axios.get(
+              'https://api.themoviedb.org/3/movie/now_playing',
+              {
+                params: {
+                  api_key: '3b24534de9f3e0c2935e3edd6446ad0c',
+                  language: 'es',
+                  page: page,
+                },
+              }
+            )
+          );
+        }
+
+        const responses = await Promise.all(requests);
+        const peliculasData = responses.flatMap(response => response.data.results);
+        setPeliculas(peliculasData.slice(0, numeroDePeliculas));
       } catch (error) {
         console.error('Error al obtener las películas:', error);
       }
     };
 
     fetchPeliculas();
-  }, []);
+  }, [numeroDePeliculas]);
 
   return (
     <div className="container">
@@ -33,7 +45,7 @@ const Cartelera = () => {
       <div className="grid">
         {peliculas.map((pelicula) => (
           <div key={pelicula.id} className="movie">
-            <a href={``}>
+            <a href={`https://www.themoviedb.org/movie/${pelicula.id}`}>
               <img
                 src={`https://image.tmdb.org/t/p/w300/${pelicula.poster_path}`}
                 alt={pelicula.title}
