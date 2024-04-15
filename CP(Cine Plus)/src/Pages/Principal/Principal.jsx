076548c0ad711cf5f4e820db57039de11efbb7a1
jp_castano <jpc4stano@gmail.com>
2024-04-14 20:15:31 -0500

    Borrando Tailwind 3 Electric Boogaloo

---
+++ CP(Cine Plus)/src/Pages/Principal/Principal.jsx
@@ -0,0 +1,13 @@
+import { Navigation } from '../../Components/Shared/BarraNavegacion.jsx'
+import Cartelera from '../Principal/Cuadricula_Prin/Cuadricula_Prin.jsx'
+
+function Principal() {
+  return (
+    <>
+      <Navigation />
+      <Cartelera></Cartelera>
+    </>
+  )
+}
+
+export default Principal
