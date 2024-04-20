1a92f673610cf033d3950d54ed3cecdf34dd6256
jp_castano <jpc4stano@gmail.com>
2024-04-20 13:13:13 -0500

    ProtoCarrusel Implementado

--- CP(Cine Plus)/src/Pages/Principal/Principal.jsx
+++ CP(Cine Plus)/src/Pages/Principal/Principal.jsx
@@ -1,10 +1,12 @@
 import { Navigation } from '../../Components/Shared/BarraNavegacion.jsx'
 import Cartelera from '../Principal/Cuadricula_Prin/Cuadricula_Prin.jsx'
+import Carrusel from './Carrusel/Carrusel.jsx'
 
 function Principal() {
   return (
     <>
       <Navigation />
+      <Carrusel/>
       <Cartelera/>
     </>
   )
