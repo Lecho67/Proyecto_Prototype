4eeb84595e1c142d0ee652e7aaf5670492b7e68e
jp_castano <jpc4stano@gmail.com>
2024-04-20 13:58:29 -0500

    Dirección a "Pelicula"

---
+++ CP(Cine Plus)/src/Pages/Pelicula_info/Pelicula_info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Navigation} from '../../Components/Shared/BarraNavegacion.jsx'
+
+const Pelicula_info = () => {
+  return (
+    <>
+      <Navigation/>
+    </>
+  )
+}
+
+export default Pelicula_info
