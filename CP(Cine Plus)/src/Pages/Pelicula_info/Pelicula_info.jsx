914c865166d48b5c9d936b0750eac9bed7dd40b7
Juan Pablo Castaño <jpc4stano@gmail.com>
2024-04-22 14:26:29 -0500

    "Pelicula_info 50% implementada"

--- CP(Cine Plus)/src/Pages/Pelicula_info/Pelicula_info.jsx
+++ CP(Cine Plus)/src/Pages/Pelicula_info/Pelicula_info.jsx
@@ -3,18 +3,23 @@ import {Navigation} from '../../Components/Shared/BarraNavegacion.jsx'
 import Portada from './Portada/Portada.jsx'
 import {useLocation} from 'react-router-dom'
 import useFetchPelicula from '../../Hooks/useFetchPelicula'
+import useFetchPeliculaCredits from '../../Hooks/useFetchPeliculaCredits'
+import Descripcion from './Descripcion/Descripcion.jsx'
 
 const Pelicula_info = () => {
   const location = useLocation()
   const queryParams = new URLSearchParams(location.search)
   const id = queryParams.get('id')
   const [pelicula, setPelicula] = useState()
+  const [credits, setCredits] = useState()
 
   const fetchPelicula = async () => {
     try {
       const data = await useFetchPelicula(id);
+      const dataCredits = await useFetchPeliculaCredits(id);
       setPelicula(data);
-      console.log(data);
+      setCredits(dataCredits);
+      console.log(dataCredits);
     }
     catch (error) {
       console.error('Error al obtener las páliculas:', error);
@@ -28,6 +33,7 @@ const Pelicula_info = () => {
     <>
       <Navigation/>
       <Portada pelicula={pelicula}/>
+      <Descripcion pelicula={pelicula} credits={credits}/>
     </>
   )
 }
