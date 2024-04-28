cb5b090009071965ac05b2527db0323e970e853c
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-28 00:09:42 -0500

    Arreglando

--- CP(Cine Plus)/src/Pages/Comidas/Productitem.jsx
+++ CP(Cine Plus)/src/Pages/Comidas/Productitem.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import styles from './styles_comida/Productitem.module.css'
-function Productitem({data,addToCart}) {
+import React from 'react';
+import styles from './styles_comida/Productitem.module.css';
+
+function Productitem({ data, addToCart }) {
   return (
-    <div className={styles.container_product}>
-      <h2>{data.name}</h2>
-      <p>Price: {data.price}</p>
-      <button className={styles.btnProduct} onClick={() => addToCart(data.id)}>Add to Cart</button>
+    <div className={styles.productContainer}> {/* Contenedor externo */}
+      <div className={styles.container_product}>
+        <h2>{data.name}</h2>
+        <p>Precio: {data.price}</p>
+        <button className={styles.btnProduct} onClick={() => addToCart(data.id)}>Agregar al Carrito</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Productitem
+export default Productitem;
