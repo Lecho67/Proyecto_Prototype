38c33e1fe4cf77ed5a3fcdb5faae0d6d2660bd9e
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-28 21:08:44 -0500

    Update main.jsx

--- CP(Cine Plus)/src/Pages/Index/main.jsx
+++ CP(Cine Plus)/src/Pages/Index/main.jsx
@@ -4,6 +4,6 @@ import App from './App.jsx'
 import { Comidas } from '../Comidas/Comidas.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Comidas/>
+    <App />
   </React.StrictMode>,
 )
