7d68d01225e1c0cf6bbc046ec9d6da4e87caddbb
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-28 21:36:34 -0500

    Update App.jsx

--- CP(Cine Plus)/src/Pages/Index/App.jsx
+++ CP(Cine Plus)/src/Pages/Index/App.jsx
@@ -2,7 +2,7 @@
 import "./App.css"
 import {BrowserRouter, Routes, Route } from "react-router-dom"
 import Principal from "../Principal/Principal"
-import Comidas from "../Comidas/Comidas"
+import {Comidas} from "../Comidas/Comidas"
 import Pelicula_info from "../Pelicula_info/Pelicula_info"
 import Estrenos from "../Estrenos/Estrenos"
 
@@ -15,7 +15,7 @@ function App() {
 					<Route path="/Comidas" element={<Comidas />} />
 					<Route path="/Pelicula" element={<Pelicula_info />} />
 					<Route path="/Estrenos" element={<Estrenos/>}/>
-					<Route path="/Estrenos" element={<Estrenos/>}/>
+					<Route path="/Comidas" element={<Comidas />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
