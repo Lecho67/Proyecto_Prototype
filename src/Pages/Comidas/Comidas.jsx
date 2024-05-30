e2be2d089e8dc121b3c241c142d974cfc03e96f7
Simon <82274800+Lecho67@users.noreply.github.com>
2024-05-29 23:29:13 -0500

    ya

--- src/Pages/Comidas/Comidas.jsx
+++ src/Pages/Comidas/Comidas.jsx
@@ -20,7 +20,7 @@ export const Comidas = ({
     useEffect(() => {
         const fetchProductos = async () => {
             try {
-                const response = await axios.get('http://localhost:4000/api/listarProductos');
+                const response = await axios.get('http://localhost:4000/listarProductos');
                 const data = Array.isArray(response.data) ? response.data : [];
                 setProductos(data);
             } catch (error) {
