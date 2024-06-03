2ff2240bd8f3dafc5104939082463909c1aa1262
jp_castano <jpc4stano@gmail.com>
2024-06-02 20:30:51 -0500

    inestable: Tratando de arreglar vuelta del login

--- src/Pages/Orden/Orden.jsx
+++ src/Pages/Orden/Orden.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import { useSelector } from 'react-redux';
 import cinePlusApi from "../../api/cinePlusApi";
 import useFetchPelicula from "../../Hooks/useFetchPelicula";
