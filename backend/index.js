4d6d34031d4e4c7b60c9651a24823ccecb086945
Juanestebane926 <juan.cuello@uao.edu.co>
2024-06-02 22:10:34 -0500

    tratando de arreglar un error de corb en el despliegue

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const {dbConnection} = require('./database/config');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const routes = require('./routes/Routes.js');
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '250mb' }));
+const bodyParser = require('body-parser');
+
+app.use( bodyParser.json({ limit:'250mb'}))
 dbConnection();
 app.use(express.static('public'));
 app.use('/api', routes);
